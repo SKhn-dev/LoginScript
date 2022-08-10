db1d3150e4a854027e0c1e53a4f9bc8bf5cc3a12
SKhn-dev <78141923+SKhn-dev@users.noreply.github.com>
2022-08-10 14:50:20 +0100

    Update LoginScript.java

--- src/LoginScript.java
+++ src/LoginScript.java
@@ -1,33 +1,35 @@
+//A program that validates the user name and password
 import java.util.Scanner;
 
 public class LoginScript {
     public static void main(String[] args) {
-        String userName="Admin";
+        String userName="Admin";  //initializing user name and password
         String password="Password!";
         int attempts=2;
-        boolean result=validateUser(userName, password);
-        while(result!=true && attempts<=3){
+        boolean result=validateUser(userName, password);  //Call to the function that validates the input
+        while(result!=true && attempts<=3){ //Repeat until the number of attempts is equal to 3 and user not validated
             attempts++;
             result=validateUser(userName,password);
 
         }
 
     }
-
+//a funtion that takes user name and password and matches it with the user input
     public static boolean validateUser(String userName,  String password){
         Scanner input=new Scanner(System.in);
         String inputName, inputPassword;
         System.out.print("Enter user name: ");
         inputName=input.nextLine();
+        if(inputName.equals(" ")||inputName.isEmpty()) //checks if the user has entered space or left the user name empty.
+        {
+            System.out.println("You must enter a user name");
+            return false;
+        }
         System.out.print("Enter password: ");
         inputPassword=input.nextLine();
         if(userName.equals(inputName) && password.equals(inputPassword)){
             System.out.println("You have been validated!");
             return true;
-        }
-        else if(inputName.equals("") || inputPassword.equals("")){
-            System.out.println("You must enter a user name");
-            return false;
         }
         else{
             System.out.println("Access not authorized");
